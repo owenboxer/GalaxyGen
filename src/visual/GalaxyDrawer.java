2afa372dec35ac3acfa1625fa3f8f5a67f73501b
adrisj7 <adrisj7@gmail.com>
2016-01-02 11:56:28 -0500

    Random Colors, just for fun

--- src/visual/GalaxyDrawer.java
+++ src/visual/GalaxyDrawer.java
@@ -3,6 +3,7 @@ package visual;
 import java.awt.*;
 //import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class GalaxyDrawer {
 	public BufferedImage image;
@@ -21,14 +22,17 @@ public class GalaxyDrawer {
 		else
 			window.setImage(image);
 	}
-	
+
 	private void drawGalaxy(BufferedImage image, int[][] density) {
 		Graphics2D g = (Graphics2D) image.getGraphics();
+		
+		float hue = (new Random()).nextFloat();
 		for(int xx = 0; xx < density.length; xx++) {
 			for(int yy = 0; yy < density[xx].length; yy++) {
-				g.setColor(new Color(255 * (int)density[xx][yy]/10,
+				g.setColor(Color.getHSBColor(hue, 1, (float)density[xx][yy]/10));
+						/*new Color(255 * (int)density[xx][yy]/10,
 						             255 * (int)density[xx][yy]/10,
-						             255 * (int)density[xx][yy]/10));
+						             255 * (int)density[xx][yy]/10));*/
 				g.fillRect(xx * rectWidth, yy * rectHeight, rectWidth, rectHeight);
 				//g.setColor(Color.BLUE);
 				//g.drawString(Integer.toString(density[xx][yy]), xx * rectWidth, (yy + 1) * rectHeight);
