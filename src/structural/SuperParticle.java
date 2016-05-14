3b4da9dd9499ec48a8ab2bcdd168ed02bd6da583
owenboxer <owencarter2000@gmail.com>
2016-05-14 09:50:37 -0400

    Adding Super Particle Class (Actually)

--- src/structural/SuperParticle.java
+++ src/structural/SuperParticle.java
@@ -1,5 +1,20 @@
 package structural;
 
 public class SuperParticle {
+	public double xx, yy;
+	public double direction, magnitude;
 
+	public final static double GRAVITATIONAL_CONSTANT = 1,
+			EXPANSION_CONSTANT = 1;
+
+	public SuperParticle(int xx, int yy){
+		this.xx = xx;
+		this.yy = yy;
+
+		genVector();
+	}
+
+	private void genVector(){
+		
+	}
 }
